afccaa1ddb5c7e7caf9b671ef18e507b045bf719
Rogério L Aguilar <rogerio.l.aguilar@gmail.com>
2016-07-23 11:40:35 -0300

    Naming container by the name of current directory.

--- run-dev.sh
+++ run-dev.sh
@@ -1,3 +1,4 @@
 #!bin/bash
-echo $(pwd)
-docker run --name rails-dev -it -p 3000:3000 -v $(pwd)/:/apps rogeriolaguilar/rails-env /bin/bash
+project_name=${PWD##*/} # current folder name without full path 
+
+docker run --name $project_name -it -p 3000:3000 -v $(pwd)/:/apps rogeriolaguilar/rails-env /bin/bash
